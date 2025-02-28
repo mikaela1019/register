70c1a6e4705854c0ee18507d18779e9932765982
mikaela1019 <mariamikaela.parayno@letran.edu.ph>
2025-03-01 01:24:18 +0800

    MainStyles.js
    update

--- src/styles/MainStyles.js
+++ src/styles/MainStyles.js
@@ -2,15 +2,47 @@ import { StyleSheet } from "react-native";
 
 export const loginstyle = StyleSheet.create (
     {
+        background: {
+            flex: 1,
+            resizeMode: 'cover', 
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
         container: {
             justifyContent: 'center',
             alignItems: 'center',
             height:'100%',
 
         },
+        helloText: {
+            color: 'white', 
+            fontSize: 30,
+            fontWeight: 'bold',
+            marginTop: 20,
+            marginBottom: 20,
+          },
+        label: {
+            alignSelf: 'flex-start',
+            marginLeft: '3%', 
+            marginBottom: 5,
+            fontWeight: 'bold',
+            fontSize: 16,
+            color: 'white',
+          },
+        TextInput: {
+            width: '280',
+            height: 50,
+            borderColor: 'gray',
+            borderWidth: 1,
+            paddingHorizontal: 10,
+            marginBottom: 15,
+            borderRadius: 50,
+            backgroundColor: 'white', 
+          },
         Imagecontainer: {
-            height:'100',
-            width:'100',
+            marginTop: 15,
+            height:'80',
+            width:'230',
 
         }
     }
