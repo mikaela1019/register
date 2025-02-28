c97ec0619a860dceee0aa278a172135e3880e6d2
mikaela1019 <mariamikaela.parayno@letran.edu.ph>
2025-03-01 01:22:56 +0800

    App.js
    update

--- App.js
+++ App.js
@@ -4,39 +4,54 @@ import { View } from "react-native";
 import { Image } from "react-native";
 import { TextInput } from "react-native";
 import { TouchableOpacity } from "react-native";
+import { ImageBackground } from "react-native";
 import { loginstyle } from "./src/styles/MainStyles";
 
 const App = () => {
   const [username, setUsername] = useState('')
-  const [firstname, setFirstName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('');
 
   const HandlePress = () => {
-    console.log('Hello')
-    setUsername('Nanami')
-    setFirstName ('Mikai')
+    console.log('Registered');
   }
   return (
+    <ImageBackground 
+      source={require('./src/assets/background.png')}  // Replace with your image path
+      style={loginstyle.background}
+    >
     <View style= {loginstyle.container} >
+    <Text style={loginstyle.helloText}>REGISTER</Text>
+    <Text style={loginstyle.label}>Username:</Text>
       <TextInput 
         value={username}
+        placeholder="Username"
         style={loginstyle.TextInput}
         onChangeText={(text) => setUsername (text)}
       />
+      <Text style={loginstyle.label}>Email:</Text>
       <TextInput 
-        value={firstname}
+        value={email}
+        placeholder="Email"
         style={loginstyle.TextInput}
-        onChangeText={(text) => setFirstName (text)}
+        onChangeText={(text) => setEmail (text)}
+      />
+      <Text style={loginstyle.label}>Password:</Text>
+      <TextInput
+        value={password}
+        placeholder="Password"
+        style={loginstyle.TextInput}
+        secureTextEntry={true} // This hides the text (password field)
+        onChangeText={(text) => setPassword(text)}
       />
       <TouchableOpacity 
         onPress= {HandlePress}>
           <Image
       style= {loginstyle.Imagecontainer}
-      source={require('./src/assets/nanami.jpeg')}/>
+      source={require('./src/assets/button.png')}/>
         </TouchableOpacity>
-       <Text>
-      Hello everyone
-      </Text>
     </View>
+    </ImageBackground>
    
   );
 
